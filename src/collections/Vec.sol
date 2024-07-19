a8540e4a1ee2eed8cd1e61f2c3342d18ba59f627
Philippe Dumonet <philippe@dumo.net>
2024-07-19 21:25:38 +0100

    ✨ Add storage vec

--- src/collections/Vec.sol
+++ src/collections/Vec.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 struct UintVec {
-    uint256 length;
+    uint256 _length;
     uint256[] _mem;
 }
 
@@ -17,20 +17,33 @@ library VecLib {
     uint256 constant MIN_NEW_CAPACITY = 1;
 
     function uint_with_cap(uint256 capacity) internal pure returns (UintVec memory obj) {
-        obj.length = 0;
+        obj._length = 0;
         obj._mem = new uint256[](capacity);
     }
 
-    function get(UintVec memory self, uint256 index) internal pure returns (uint256 value) {
-        if (index >= self.length) revert OutOfBoundsVecGet();
+    function length(UintVec memory self) internal pure returns (uint256) {
+        return self._length;
+    }
+
+    function length(UintVec storage self) internal view returns (uint256) {
+        return self._mem.length;
+    }
+
+    function get(UintVec memory self, uint256 index) internal pure returns (uint256) {
+        if (index >= self._length) revert OutOfBoundsVecGet();
+        return self._mem[index];
+    }
+
+    function get(UintVec storage self, uint256 index) internal view returns (uint256) {
+        if (index >= self._mem.length) revert OutOfBoundsVecGet();
         return self._mem[index];
     }
 
     function push(UintVec memory self, uint256 value) internal pure {
-        uint256 index = self.length;
-        self.length++;
+        uint256 index = self._length;
+        self._length++;
         uint256 oldCap = self._mem.length;
-        if (self.length > oldCap) {
+        if (self._length > oldCap) {
             uint256 newCap = oldCap * 2;
             uint256[] memory newMem = new uint256[](newCap < MIN_NEW_CAPACITY ? MIN_NEW_CAPACITY : newCap);
             for (uint256 i = 0; i < oldCap; i++) {
@@ -41,9 +54,7 @@ library VecLib {
         self._mem[index] = value;
     }
 
-    // function contains(UintVec memory self, uint value) internal pure returns (bool) {
-    //     for (uint256 i = 0; i < self.length; i++) {
-    //         if (self._mem[])
-    //     }
-    // }
+    function push(UintVec storage self, uint256 value) internal {
+        self._mem.push(value);
+    }
 }
