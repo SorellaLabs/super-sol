3dbe72b3df98f55bfd704718ac132ae0946f8086
Philippe Dumonet <philippe@dumo.net>
2024-07-29 13:42:55 +0200

    ✨ Add bounds method

--- src/collections/CalldataReader.sol
+++ src/collections/CalldataReader.sol
@@ -7,9 +7,22 @@ using CalldataReaderLib for CalldataReader global;
 
 /// @author philogy <https://github.com/philogy>
 library CalldataReaderLib {
+    error ReaderOutOfBounds();
+
     function from(bytes calldata data) internal pure returns (CalldataReader reader) {
         assembly {
             reader := data.offset
+        }
+    }
+
+    function notAtEnd(CalldataReader self, bytes calldata data) internal pure returns (bool notEnded) {
+        assembly {
+            let end := add(data.offset, data.length)
+            notEnded := lt(self, end)
+            if gt(self, end) {
+                mstore(0x00, 0x353a325a /* ReaderOutOfBounds() */ )
+                revert(0x1c, 0x04)
+            }
         }
     }
 
@@ -19,33 +32,33 @@ library CalldataReaderLib {
         }
     }
 
-    function readBool(CalldataReader reader) internal pure returns (bool value) {
+    function readBool(CalldataReader self) internal pure returns (bool value) {
         assembly {
-            value := byte(0, calldataload(reader))
+            value := byte(0, calldataload(self))
         }
     }
 
-    function readU16(CalldataReader reader) internal pure returns (uint16 value) {
+    function readU16(CalldataReader self) internal pure returns (uint16 value) {
         assembly {
-            value := shr(240, calldataload(reader))
+            value := shr(240, calldataload(self))
         }
     }
 
-    function readU40(CalldataReader reader) internal pure returns (uint40 value) {
+    function readU40(CalldataReader self) internal pure returns (uint40 value) {
         assembly {
-            value := shr(216, calldataload(reader))
+            value := shr(216, calldataload(self))
         }
     }
 
-    function readU64(CalldataReader reader) internal pure returns (uint64 value) {
+    function readU64(CalldataReader self) internal pure returns (uint64 value) {
         assembly {
-            value := shr(192, calldataload(reader))
+            value := shr(192, calldataload(self))
         }
     }
 
-    function readU128(CalldataReader reader) internal pure returns (uint128 value) {
+    function readU128(CalldataReader self) internal pure returns (uint128 value) {
         assembly {
-            value := shr(128, calldataload(reader))
+            value := shr(128, calldataload(self))
         }
     }
 }
