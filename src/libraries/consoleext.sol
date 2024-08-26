325f1dc6d9d53caf1dc83f3b2401f627811ba236
philogy <philippe@dumo.net>
2024-08-26 10:38:05 +0300

    ✨ Update consoleext

--- src/libraries/consoleext.sol
+++ src/libraries/consoleext.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.0;
 
 import {console} from "forge-std/console.sol";
+import {safeconsole} from "forge-std/safeconsole.sol";
 import {LibString} from "solady/src/utils/LibString.sol";
 
 /// @author philogy <https://github.com/philogy>
 library consoleext {
     using LibString for uint256;
 
-    function logWords(bytes memory data) public pure {
+    function logInWords(bytes memory data) public pure {
         for (uint256 i = 0; i < data.length; i += 32) {
             uint256 word;
             assembly ("memory-safe") {
@@ -17,6 +18,14 @@ library consoleext {
             uint256 len = data.length - i;
             if (len > 32) len = 32;
             console.log("%s", word.toHexString(len));
+        }
+    }
+
+    function logMemWords(uint256 startOffset, uint256 endOffset) public pure {
+        for (uint256 offset = startOffset; offset < endOffset; offset += 32) {
+            uint256 wordEnd = offset + 32;
+            if (wordEnd > endOffset) wordEnd = endOffset;
+            safeconsole.logMemory(offset, wordEnd - offset);
         }
     }
 }
