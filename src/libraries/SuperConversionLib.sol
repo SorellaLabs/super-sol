54d5610fcb0511bee2a1f86f80eeb1732977f7b3
philogy <philippe@dumo.net>
2024-09-20 23:45:17 +0100

    ✨ SuperConversionLib

---
+++ src/libraries/SuperConversionLib.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @author philogy <https://github.com/philogy>
+library SuperConversionLib {
+    function into(uint256[1] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[2] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[3] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[4] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[5] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[6] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[7] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[8] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[9] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[10] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[11] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[12] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[13] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[14] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[15] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[16] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[17] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[18] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[19] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[20] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[21] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[22] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[23] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[24] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+
+    function into(uint256[25] memory arr) internal pure returns (uint256[] memory) {
+        return abi.decode(abi.encode(uint256(0x20), arr.length, arr), (uint256[]));
+    }
+}
